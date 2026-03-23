a0fe67bd0a18d6ea126b251bcaa0558bbec8768c
dela-maza <touki_sigotoyou@yahoo.co.jp>
2026-03-23 23:47:18 +0900

    3

--- common.py
+++ common.py
@@ -1,5 +1,5 @@
 # core/registry/real_estate/schema/frame/common.py
-test!!!!!!!!!
+test222222!!!!!!!!!
 from core.registry.common.constants import MARGIN,DELETE
 def is_valid_value(v: str) -> bool:
     """余白記号もしくは抹消記号の場合はFalseを返す"""
